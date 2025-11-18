0ed0c738909d86938217272ecf23424d14d4e39e
Shriansh Singh Jaswal <shrianshsinghjaswal@gmail.com>
2025-11-18 14:08:50 +0530

    Update ApiGatewayApplicationTests.java

--- src/test/java/com/example/APIGateway/ApiGatewayApplicationTests.java
+++ src/test/java/com/example/APIGateway/ApiGatewayApplicationTests.java
@@ -2,7 +2,7 @@ package com.example.APIGateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//changed in GitHub
 @SpringBootTest
 class ApiGatewayApplicationTests {
 
